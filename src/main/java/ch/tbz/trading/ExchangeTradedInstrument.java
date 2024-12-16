2794a7f7674c8ee86d6f25e851d385c88ee47133
Nic <48014106+duneau@users.noreply.github.com>
2024-12-16 12:30:04 +0000

    with javadocs for the ExchangeTradedInstrument class

--- src/main/java/ch/tbz/trading/ExchangeTradedInstrument.java
+++ src/main/java/ch/tbz/trading/ExchangeTradedInstrument.java
@@ -1,17 +1,31 @@
 /**
- * Created: Dec 15, 2024
- * @version: 1.0
- * @author: Nicolas Duneau
- */
+ * This abstract class represents an exchange-traded instrument (ETI). An ETI is a security that is traded on a stock exchange.
+ * This class provides common properties and methods for all ETIs, such as symbol and name.
+ * @since 1.0
+ * @author Nicolas Duneau 
+ */ 
+
 package ch.tbz.trading;
 
 public abstract class ExchangeTradedInstrument {
     
-
+    /**
+     * The symbol of the ETI. The symbol is a unique identifier for the ETI on the exchange.
+     */
     private String symbol;
+
+    /**
+     * The name of the ETI. The name is a human-readable description of the ETI.
+     */
     private String name;
 
-    // constructor
+    /**
+     * Creates a new ExchangeTradedInstrument object.
+     * 
+     * @param symbol the symbol of the ETI
+     * @param name the name of the ETI
+     */
+
     public ExchangeTradedInstrument(String symbol, String name) {
         this.symbol = symbol;
         this.name = name;
